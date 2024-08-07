af9b34f5796391464ce7eb93953408e60118a99a
dumblek <duharamadhani@gmail.com>
2024-08-07 09:46:25 +0700

    edit manifest

--- crm_probability_from_stage/__manifest__.py
+++ crm_probability_from_stage/__manifest__.py
@@ -6,16 +6,16 @@
         enabling more accurate sales forecasting and better decision-making throughout the sales process.""",
 
     'description': """
-        This module allows for the manual assignment of probability values to each stage in the CRM pipeline. 
-        By defining specific probabilities for various sales stages, the module ensures that the probability field in CRM leads automatically aligns with the stage’s predefined probability. 
-        This approach enhances forecasting accuracy and supports sales teams in making informed decisions, ultimately improving overall sales performance and efficiency.
+        module automates the assignment of probability values to each stage in the CRM pipeline. 
+        By mapping specific probabilities to various sales stages, this module enhances forecasting accuracy and aids sales teams in making informed decisions, 
+        ultimately improving overall sales performance and efficiency.
     """,
     'author': "Doodex",
     'website': "https://www.doodex.net/",
     'category': 'Marketing/Sales',
-    'version': '16.0.1',
+    'version': '16.0.1.0',
     'depends': ['base','crm'],
-    'images': ["static/description/icon.png"],
+    'images': ["static/description/banner.png",],
     'license': 'LGPL-3',
     'data': [
         # 'security/ir.model.access.csv',
@@ -23,5 +23,7 @@
         'views/res_config_settings.xml',
     ],
     'application': True,
-    'installable': True
+    'installable': True,
+    "price": 5,
+    "currency": "USD",
 }
